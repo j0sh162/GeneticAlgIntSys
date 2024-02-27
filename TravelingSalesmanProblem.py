4aff11ebe29c5957698b767e370fdd0b5ccfc195
j0sh162 <joshkramer567@gmail.com>
2024-02-27 09:23:41 +0100

    crossover GA should be done but havent tested

--- TravelingSalesmanProblem.py
+++ TravelingSalesmanProblem.py
@@ -1,41 +1,86 @@
-import sys
+import random
 
-def tsp(graph, start):
-    n = len(graph)
-    # Initialize memoization table with -1
-    memo = [[-1] * (1 << n) for _ in range(n)]
 
-    # Helper function to solve TSP recursively
-    def tsp_helper(curr, visited):
-        if visited == (1 << n) - 1:  # All cities have been visited
-            return graph[curr][start]
+# Each chromosone is the order of the cities we visit
+def gen_genome(len):
+    genome = []
+    while True:
+        rnd = random.randint(0, len)
+        if rnd not in genome:
+            genome.append(rnd)
+        if len(genome) == len:
+            return {"score": 0, "genome": genome}
+
+
+def gen_population(pop_size, len):
+    population = []
+    for i in range(pop_size):
+        population.append(gen_genome(len))
+    return population
+
+
+def genetic_alg(pop_size, map, iter):
+    population = gen_population(pop_size,len(map[0]))
+    
+    for i in range(iter):
+        population = evaluate_func(population,map)
+        population = crossover(population)
+    
+    return None
+
+
+def evaluate_func(population, map):
+    for genome in population:
+        f = 0
+        for i in range(len(genome) - 1):
+            f += map[genome[i]][genome[i] + 1]
+        genome['score'] = f
+    return sorted(population, key=lambda x: x['score'],reverse=True)
+
+
+# for this we take the top half of the population and breed them to fill up the population
+def crossover(population):
+    population = population[:len(population)/2]
+    children = []    
+    for i in range(0,len(population),2):
+        dad = population[i]['genome']
+        mom = population[i+1]['genome']
         
-        if memo[curr][visited] != -1:  # Check if result already exists in memo
-            return memo[curr][visited]
+        point1 = random.randint(0, len(dad))
+        point2 = random.randint(0, len(dad))
 
-        min_cost = sys.maxsize
-        for city in range(n):
-            if (visited >> city) & 1 == 0:  # Check if city has not been visited
-                cost = graph[curr][city] + tsp_helper(city, visited | (1 << city))
-                min_cost = min(min_cost, cost)
+        if(point2 < point1):
+            point1,point2 = point2,point1
+        
+        child1 = dad[point1:point2]
+        child2 = dad[:point1]
+        child2.append(dad[point2:])
+        for j in mom:
+            if j not in child1:
+                child1.append(j)
+            if j not in child2:
+                child2.append(j)
+        
+        child1 = {
+            'score':0,
+            'genome':child1
+        }
+        child2 = {
+            'score':0,
+            'genome':child2
+        }
+        children.append([child1,child2])
+    
+    population.append(children)    
+        
+        
+    return population
 
-        memo[curr][visited] = min_cost
-        return min_cost
 
-    # Start with the first city and visit all other cities
-    return tsp_helper(start, 1 << start)
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Example usage:
-    graph = [
-        [0, 10, 15, 20],
-        [10, 0, 35, 25],
-        [15, 35, 0, 30],
-        [20, 25, 30, 0]
-    ]
+    graph = [[0, 10, 15, 20], [10, 0, 35, 25], [15, 35, 0, 30], [20, 25, 30, 0]]
     start_city = 0
+    min_cost = 0
 
-    min_cost = tsp(graph, start_city)
     print("Minimum cost of traveling salesman tour:", min_cost)
