8490841950e4bc31f03a73634f3e37eaac6c523c
j0sh162 <joshkramer567@gmail.com>
2024-02-28 15:55:54 +0100

    GA works :)

--- TravelingSalesmanProblem.py
+++ TravelingSalesmanProblem.py
@@ -5,33 +5,34 @@ INT_MAX = 2147483647
 # Each chromosone is the order of the cities we visit
 
 #TODO define a start postion as it needs to be a cycle
-def gen_genome(leng):
+def gen_genome(leng,start):
     genome = []
     while True:
         rnd = random.randint(0, leng-1)
-        if rnd not in genome:
+        if rnd not in genome and rnd != start:
             genome.append(rnd)
-        if len(genome) == leng:
+        if len(genome) == leng-1:
+            genome = [start]+genome+[start]
             return {"score": 0, "genome": genome}
 
 
-def gen_population(pop_size, len):
+def gen_population(pop_size, len,start):
     population = []
     for i in range(pop_size):
         # print(population)
-        population.append(gen_genome(len))
+        population.append(gen_genome(len,start))
     return population
 
 
-def genetic_alg(pop_size, map, iter):
-    population = gen_population(pop_size, len(map[0]))
-    # print(population)
+def genetic_alg(pop_size, map, iter,start = 0):
+    population = gen_population(pop_size, len(map[0]),start)
+    print(population)
     # print(len(population))
     for i in range(iter):
         print(i)
-        population = evaluate_func(population, map)[:10]
+        population = evaluate_func(population, map)[:int(pop_size/2)]
         # population = roulette_selection(population)
-        population = crossover(population)
+        population = crossover(population,start)
         population = mutate(population)
     population = evaluate_func(population, map)
     
@@ -68,8 +69,9 @@ def mutate(population, p=0.2):
         # print(i)
         rnd = random.random()
         if(rnd<p):
-            rnd_ind1 = random.randint(0,len(i['genome'])-1)
-            rnd_ind2 = random.randint(0,len(i['genome'])-1)
+            
+            rnd_ind1 = random.randint(1,len(i['genome'])-2)
+            rnd_ind2 = random.randint(1,len(i['genome'])-2)
             
             i['genome'][rnd_ind1], i['genome'][rnd_ind2] = i['genome'][rnd_ind2], i['genome'][rnd_ind1]
             
@@ -78,11 +80,11 @@ def mutate(population, p=0.2):
 
 
 # for this we take the top half of the population and breed them to fill up the population
-def crossover(population):
+def crossover(population,start):
     children = []
     for i in range(0, len(population), 2):
-        dad = population[i]["genome"]
-        mom = population[i + 1]["genome"]
+        dad = population[i]["genome"][1:-1]
+        mom = population[i + 1]["genome"][1:-1]
 
         point1 = random.randint(0, len(dad)-1)
         point2 = random.randint(0, len(dad)-1)
@@ -98,7 +100,10 @@ def crossover(population):
                 child1.append(j)
             if j not in child2:
                 child2.append(j)
-
+        
+        child1 = [start]+child1+[start]
+        child2 = [start]+child2+[start]
+        
         child1 = {"score": 0, "genome": child1}
         child2 = {"score": 0, "genome": child2}
         children = children + [child1,child2]
@@ -118,4 +123,6 @@ if __name__ == "__main__":
         [5, INT_MAX, 3, 10, 0],
     ]
     print(genetic_alg(20,graph,100))
+    
+    
     min_cost = 0
