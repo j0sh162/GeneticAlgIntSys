6356c2ee97e7e618bedb0897c59d0f8a3a0ff774
Illarionis <olexander.finkov@gmail.com>
2024-03-21 14:15:34 +0100

    work in progress

--- TravelingSalesmanProblem.py
+++ TravelingSalesmanProblem.py
@@ -1,10 +1,21 @@
 import random
 import numpy as np
+import csv
 
 INT_MAX = 2147483647
 
-# Each chromosone is the order of the cities we visit
+def generate_city(num_cities):
+    # Generate random cost matrix for 50 cities
+    cost_matrix = np.random.randint(1, 50, size=(num_cities, num_cities))
 
+    # Make diagonal elements zero since it costs nothing to go from a city to itself
+    cost_matrix = cost_matrix + cost_matrix.T
+    np.fill_diagonal(cost_matrix, 0)
+
+    # Display the cost matrix
+    return cost_matrix
+
+# Each chromosone is the order of the cities we visit
 def gen_genome(leng,start):
     genome = []
     while True:
@@ -24,21 +35,6 @@ def gen_population(pop_size, len,start):
     return population
 
 
-def genetic_alg(pop_size, map, iter,start = 0):
-    population = gen_population(pop_size, len(map[0]),start)
-    print(population)
-    # print(len(population))
-    for i in range(iter):
-        print(i)
-        population = evaluate_func(population, map)[:int(pop_size/2)]
-        # population = roulette_selection(population)
-        population = crossover(population,start)
-        population = mutate(population)
-    population = evaluate_func(population, map)
-    
-    return population[0]
-
-
 def evaluate_func(population, map):
     # print(len(population))
     for genome in population:
@@ -51,87 +47,146 @@ def evaluate_func(population, map):
         genome['score'] = f
         # print(genome)
        
-    return sorted(population, key=lambda x: x["score"], reverse=False)
+    # return sorted(population, key=lambda x: x["score"], reverse=True)
+    return population
 
+# Tournamnet selection, chooses two most fittest individuals
+def tournament_selection(population, tournament_size):
+    selected_inds = []
 
-def roulette_selection(population, prob_thresh=0.5):
-    selection = []
-    total_value = sum(item["score"] for item in population)
-    for i in range(len(population)):
-        if population[i]["score"] / total_value >= prob_thresh:
-            selection.append(population[i])
-    return selection
+    # several limited size tournaments
+    # here we are doing minimization,
+    # so the lower the score - the fitter the individual
+    for _ in range(len(population)//2):
+        tournament = random.sample(population, tournament_size)
+        tournament_fitness = [ind["score"] for ind in tournament]
+
+        # select the fittest ind
+        fittest_individual_index = tournament_fitness.index(min(tournament_fitness))
+        selected_inds.append(tournament[fittest_individual_index])
+        
+        # select the 2nd fittest ind
+        tournament_fitness[fittest_individual_index] = float('inf') # setting the 1st fittest to not be selected with the max
+        fittest_individual_index = tournament_fitness.index(min(tournament_fitness))
+        selected_inds.append(tournament[fittest_individual_index])
+
+    return selected_inds
 
 
 def mutate(population, p=0.2):
-   
     for i in population:
         # print(i)
         rnd = random.random()
-        if(rnd<p):
-            
+        if(rnd < p):
             rnd_ind1 = random.randint(1,len(i['genome'])-2)
             rnd_ind2 = random.randint(1,len(i['genome'])-2)
             
             i['genome'][rnd_ind1], i['genome'][rnd_ind2] = i['genome'][rnd_ind2], i['genome'][rnd_ind1]
             
-    
     return population
 
+def crossover_ind(mom, dad, start):
+    dad = dad["genome"][1:-1]
+    mom = mom["genome"][1:-1]
 
-# for this we take the top half of the population and breed them to fill up the population
-def crossover(population,start):
+    point1 = random.randint(0, len(dad)-1)
+    point2 = random.randint(0, len(dad)-1)
+    if point2 < point1:
+        point1, point2 = point2, point1
+
+    child1 = dad[point1:point2]
+    child2 = dad[:point1] + dad[point2:]
+    
+    for j in mom:
+        if j not in child1:
+            child1.append(j)
+        if j not in child2:
+            child2.append(j)
+    
+    child1 = [start]+child1+[start]
+    child2 = [start]+child2+[start]
+    
+    child1 = {"score": 0, "genome": child1}
+    child2 = {"score": 0, "genome": child2}
+
+    return child1, child2 
+    
+def crossover(population, start, crossover_rate):
     children = []
-    for i in range(0, len(population), 2):
-        dad = population[i]["genome"][1:-1]
-        mom = population[i + 1]["genome"][1:-1]
-
-        point1 = random.randint(0, len(dad)-1)
-        point2 = random.randint(0, len(dad)-1)
-
-        if point2 < point1:
-            point1, point2 = point2, point1
-
-        child1 = dad[point1:point2]
-        child2 = dad[:point1] + dad[point2:]
-        
-        for j in mom:
-            if j not in child1:
-                child1.append(j)
-            if j not in child2:
-                child2.append(j)
-        
-        child1 = [start]+child1+[start]
-        child2 = [start]+child2+[start]
-        
-        child1 = {"score": 0, "genome": child1}
-        child2 = {"score": 0, "genome": child2}
-        children = children + [child1,child2]
-
-    population = population + children
-
-    return population
-
-
-def generate_city(num_cities):
-    np.random.seed(0)
-    # Set random seed for reproducibility
     
+    while len(children) < len(population):
+        p1_id = random.randint(0, len(population)-1)
+        p2_id = random.randint(0, len(population)-1)
+        while p2_id == p1_id:
+            p2_id = random.randint(0, len(population)-1)
+        
+        parent1 = population[p1_id]
+        parent2 = population[p2_id]  
 
-    # Generate random cost matrix for 50 cities
-    cost_matrix = np.random.randint(1, 50, size=(num_cities, num_cities))
+        if random.random() <= crossover_rate:
+            child1, child1 = crossover_ind(parent1, parent2, start)
+            children.append(child1)
+            children.append(child1)
+        else:
+            children.append(parent1)
+            children.append(parent2)
 
-    # Make diagonal elements zero since it costs nothing to go from a city to itself
-    cost_matrix = cost_matrix + cost_matrix.T
-    np.fill_diagonal(cost_matrix, 0)
+    return children
 
-    # Display the cost matrix
-    return cost_matrix
+def select_best(population):
+    sorted_pop = sorted(population, key=lambda x: x["score"], reverse=False)
+    return sorted_pop[0]
 
 
+def genetic_alg(map, pop_size, tournament_size, crossover_rate, mutation_rate, iter, start = 0):
+    population = gen_population(pop_size, len(map[0]),start)
+    best_solution = {"score": INT_MAX, "genome": ""}
+    gen_solution_found = 0
+    
+    # Collect data for CSV
+    results = []
+
+
+    population = evaluate_func(population, map)
+    for i in range(iter):
+        population = tournament_selection(population, tournament_size)
+        population = crossover(population, start, crossover_rate)
+        population = mutate(population, mutation_rate)
+        population = evaluate_func(population, map)
+        
+        tmp_best = select_best(population)
+        if tmp_best["score"] < best_solution["score"]:
+            best_solution = tmp_best.copy()
+            gen_solution_found = i
+        
+        # Append data for CSV
+        results.append({
+            "Generation": i,
+            "Best Score": best_solution["score"],
+            "Best Genotype": best_solution["genome"],
+            "Population Size": pop_size,
+            "Tournament Size": tournament_size,
+            "Crossover Rate": crossover_rate,
+            "Mutation Rate": mutation_rate,
+            "Iterations": gen_iter
+        })
+
+
+    return best_solution, gen_solution_found, results
+
+def save_results_to_csv(results, filename):
+    with open(filename, 'w', newline='') as csvfile:
+        fieldnames = results[0].keys()
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(results)
 
 if __name__ == "__main__":
+    np.random.seed(0)
+    random.seed(404)
+    
     # Example usage:
+    graph = generate_city(100)
     # graph = [
     #     [0, 2, INT_MAX, 12, 5],
     #     [2, 0, 4, 8, INT_MAX],
@@ -139,8 +194,19 @@ if __name__ == "__main__":
     #     [12, 8, 3, 0, 10],
     #     [5, INT_MAX, 3, 10, 0],
     # ]
-    graph = generate_city(50)
-    print(genetic_alg(1000,graph,10000))
-    print(graph)
+    pop_size = 500
+    gen_iter = 1000
     
-    min_cost = 0
+    tournament_size = pop_size//10
+    crossover_rate = 0.5
+    mutation_rate = 0.1
+    
+    best_solution, gen_solution_found, results = genetic_alg(graph, pop_size, tournament_size, crossover_rate, mutation_rate, gen_iter)
+
+    print("Best Solution Found:", best_solution)
+    print("Generation Solution Found:", gen_solution_found)
+    print(graph)
+
+    save_results_to_csv(results, "TSP_GA_results.csv")
+
+    
--- knapsack_genetic.py
+++ knapsack_genetic.py
@@ -1,4 +1,6 @@
 import random
+import csv
+
 # generates a list of values for knapsack items as a range: [1, n]
 # return list is of length count_times (the amount of knapsack items)
 def generate_values(count_items):
@@ -85,28 +87,8 @@ def tournament_selection(population, tournament_size):
 
     return selected_inds
 
-# bit flipping mutation
-def mutation(genotype, mutation_rate):
-    for i in range(len(genotype)):
-        if random.random() <= mutation_rate:
-            if int(genotype[i]) == 1:
-                genotype = change_str_char_at_i(genotype, '0', i)
-            else:
-                genotype = change_str_char_at_i(genotype, '1', i)
-    return genotype
-
-# utility function for mutation()
-def change_str_char_at_i(string, char, i):
-    return string[:i] + char + string[i+1:]
-
-# apply mutation to the whole population
-def mutate_population(population, mutation_rate):
-    for ind in population:
-        ind["genotype"] = mutation(ind["genotype"], mutation_rate)
-    return population
-
 # double-point crossover
-def crossover(mom, dad):
+def crossover_ind(mom, dad):
     mom_genotype = mom["genotype"]
     dad_genotype = dad["genotype"]
     
@@ -123,10 +105,71 @@ def crossover(mom, dad):
 
     return kid1, kid2
 
+def crossover_population(population, crossover_rate):
+    kids = []
+    while len(kids) < len(population):
+        p1_id = random.randint(0, len(population)-1)
+        p2_id = random.randint(0, len(population)-1)
+        while p2_id == p1_id:
+            p2_id = random.randint(0, len(population)-1)
+        
+        parent1 = population[p1_id]
+        parent2 = population[p2_id]
+        
+        if random.random() <= crossover_rate:
+            kid1, kid2 = crossover_ind(parent1, parent2)
+            kids.append(kid1)
+            kids.append(kid2)
+        else:
+            kids.append(parent1)
+            kids.append(parent2)
+    return kids
+
+# apply bit-flipping mutation to the whole population
+def mutate(population, mutation_rate):
+    for ind in population:
+        genotype = ind["genotype"]
+        for i in range(len(genotype)):
+            if random.random() <= mutation_rate:
+                if int(genotype[i]) == 1:
+                    genotype = genotype[:i] + '0' + genotype[i+1:]
+                else:
+                    genotype = genotype[:i] + '1' + genotype[i+1:]
+        ind["genotype"] = genotype
+    return population
+
 # Selects the ind with the highest fitness value
 def select_best(population):
     sorted_pop = sorted(population, key=lambda x: x["value"], reverse=True)
     return sorted_pop[0]
+
+# def genetic_main_loop(max_weight, values, weights, pop_size, tournament_size, crossover_rate, mutation_rate, gen_iter):
+#     count_items = len(values)
+#     population = generate_population(count_items, pop_size)
+    
+#     best_solution = {"genotype": "", "value": 0, "weight": 0}
+#     gen_solution_found = 0
+    
+#     population = evaluate_fitness(population, count_items, max_weight, weights, values)
+#     for i in range(gen_iter): 
+#         # Selection (tournamnet)
+#         population = tournament_selection(population, tournament_size)
+#         # Crossover - reproduction
+#         population = crossover_population(population, crossover_rate)
+#         # Mutate
+#         population = mutate(population, mutation_rate)
+#         # Evaluate, save the best solution thus far
+#         population = evaluate_fitness(population, count_items, max_weight, weights, values)
+#         tmp_best = select_best(population)
+#         if tmp_best["value"] > best_solution["value"]:
+#             best_solution = tmp_best
+#             gen_solution_found = i
+
+#     return best_solution, gen_solution_found
+
+def get_avg(population):
+    values = [p["value"] for p in population ]
+    return sum(values)/len(population)
 
 def genetic_main_loop(max_weight, values, weights, pop_size, tournament_size, crossover_rate, mutation_rate, gen_iter):
     count_items = len(values)
@@ -135,56 +178,80 @@ def genetic_main_loop(max_weight, values, weights, pop_size, tournament_size, cr
     best_solution = {"genotype": "", "value": 0, "weight": 0}
     gen_solution_found = 0
     
-    population = evaluate_fitness(population, count_items, max_weight, weights, values)
+    # Collect data for CSV
+    results = []
     
+    population = evaluate_fitness(population, count_items, max_weight, weights, values)
     for i in range(gen_iter): 
         # Selection (tournamnet)
-        selected_inds = tournament_selection(population, tournament_size)
-        
+        population = tournament_selection(population, tournament_size)
         # Crossover - reproduction
-        kids = []
-        while len(kids) < len(population):
-            p1_id = random.randint(0, len(selected_inds)-1)
-            p2_id = random.randint(0, len(selected_inds)-1)
-            while p2_id == p1_id:
-                p2_id = random.randint(0, len(selected_inds)-1)
-            
-            parent1 = selected_inds[p1_id]
-            parent2 = selected_inds[p2_id]
-            
-            if random.random() <= crossover_rate:
-                kid1, kid2 = crossover(parent1, parent2)
-                kids.append(kid1)
-                kids.append(kid2)
-            else:
-                kids.append(parent1)
-                kids.append(parent2)
-        
+        population = crossover_population(population, crossover_rate)
         # Mutate
-        population = mutate_population(kids, mutation_rate)
-        
+        population = mutate(population, mutation_rate)
         # Evaluate, save the best solution thus far
         population = evaluate_fitness(population, count_items, max_weight, weights, values)
-        
         tmp_best = select_best(population)
         if tmp_best["value"] > best_solution["value"]:
-            best_solution = tmp_best
+            best_solution = tmp_best.copy()
             gen_solution_found = i
+        
+        # Append data for CSV
+        results.append({
+            "Generation": i,
+            "Best Value": best_solution["value"],
+            "Average Value": get_avg(population),
+            "Best Weight": best_solution["weight"],
+            "Best Genotype": best_solution["genotype"],
+            "Max Weight": max_weight,
+            "Values": values,
+            "Weights": weights,
+            "Population Size": pop_size,
+            "Tournament Size": tournament_size,
+            "Crossover Rate": crossover_rate,
+            "Mutation Rate": mutation_rate,
+            "Iterations": gen_iter
+        })
 
-    return best_solution, gen_solution_found
+    
+    return best_solution, gen_solution_found, results, select_best(population)
+
+def save_results_to_csv(results, filename):
+    with open(filename, 'w', newline='') as csvfile:
+        fieldnames = results[0].keys()
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(results)
 
 if __name__ == "__main__":
     random.seed(404)
 
-    max_weight = 20
-    values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    weights = [2, 8, 3, 8, 1, 9, 3, 6, 5, 4]
-
+    max_weight = 60
+    # values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+    # weights = [2, 7, 1, 9, 2, 10, 2, 6, 3, 7, 2, 1, 8, 2, 9, 1, 3, 10, 3, 4, 3, 9, 3, 5, 7, 5, 9, 1, 5, 6]
+    values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50]
+    weights = [2, 7, 1, 9, 2, 10, 2, 6, 3, 7, 2, 1, 8, 2, 9, 1, 3, 10, 3, 4, 3, 9, 3, 5, 7, 5, 9, 1, 5, 6, 9, 10, 4, 2, 1, 5, 2, 7, 8, 7, 10, 3, 9, 7, 2, 9, 4, 5, 1, 9]
     pop_size = 100
-    tournament_size = pop_size//10
+    gen_iter = 1000
+    
+    
+    # print(generate_values(50))
+    # print(generate_weights(50))
+
+    # max_weight = 20
+    # values =    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    # weights =   [2, 8, 3, 8, 1, 9, 3, 6, 5, 4]
+    # pop_size = 100
+    # gen_iter = 100
+
+    tournament_size = pop_size // 10
     crossover_rate = 0.5
-    mutation_rate = 0.1
-    gen_iter = 100
-    print("values:", values)
-    print("weights:", weights)
-    print(genetic_main_loop(max_weight, values, weights, pop_size, tournament_size, crossover_rate, mutation_rate, gen_iter))
+    mutation_rate = 0.05
+
+    best_solution, gen_solution_found, results, best_in_pop = genetic_main_loop(max_weight, values, weights, pop_size, tournament_size, crossover_rate, mutation_rate, gen_iter)
+
+    print("Best Solution Found:", best_solution)
+    print("Generation Solution Found:", gen_solution_found)
+    print("Best in Last Population:", best_in_pop)
+
+    save_results_to_csv(results, "knapsack_GA_results.csv")
